5793c66c3f1971c5d30ff0a1eb57502a1246e92a
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2022-11-02 23:04:04 +0100

    Added some functionality
    Added a new class Question and created questions[].
    I also added setQuestionCounter() to skip to the next question when tapping the true or false button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(const Quizzler());
 
@@ -24,33 +25,57 @@ class Quizzler extends StatelessWidget {
 class QuizPage extends StatefulWidget {
   const QuizPage({super.key});
 
-  final List checkList = const [];
-
-  List checkAnswer() {
-    //checking if answer is true or false
-    //return icon
-  }
-
   @override
   _QuizPageState createState() => _QuizPageState();
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int questionCounter = 0;
+
+  final List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: false),
+    Question(q: 'A slug\'s blood is green.', a: true),
+  ];
+
+  final List<Icon> answers = const [];
+
+  // List checkAnswer() {
+  //checking if answer is true or false
+  //return icon in checkList
+  // }
+
+  void setQuestionCounter() {
+    setState(() {
+      if (questionCounter < questions.length - 1) {
+        setState(() {
+          questionCounter++;
+        });
+      } else {
+        setState(() {
+          questionCounter = 0;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        const Expanded(
+        Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionCounter].questionText,
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
@@ -72,7 +97,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setQuestionCounter();
               },
             ),
           ),
@@ -90,7 +115,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setQuestionCounter();
               },
             ),
           ),
@@ -107,9 +132,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
